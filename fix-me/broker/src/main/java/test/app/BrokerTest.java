d7d0f0788af8d3cf9ce2d47cd62d696677133027
kmarchan <kmarchan@student.wethinkcode.co.za>
2019-09-13 19:05:17 +0200

    testing not working

---
+++ fix-me/broker/src/main/java/test/app/BrokerTest.java
@@ -0,0 +1,35 @@
+package test.app; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Broker Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 11, 2019</pre> 
+* @version 1.0 
+*/ 
+public class BrokerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
